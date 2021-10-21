510ba91e475bff15dbb5e116ded31d83267a2c23
Jussi Salokanta <jussi.salokanta@gmail.com>
2021-10-21 12:31:31 +0300

    Modified pages and info-modal added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
+import Modal from './modal/modal';
+import Completed from './pages/completed'
+import Home from './pages/home'
 import React from 'react';
+import {useState} from 'react';
 import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
 
-class App extends React.Component {
-  render() {
-    return (
-      <BrowserRouter>
+function App() {
+  const [show, setShow] = useState(false)
+  return (
+          <BrowserRouter>
         <div className="App">
           <nav className="nav-bar">
             <ul>
@@ -16,25 +20,19 @@ class App extends React.Component {
                 <Link to="/completed">Completed</Link>
               </li>
               <li className="nav-item">
-                <Link to="/info">Info</Link>
+              <button className="infobutton" onClick={() => setShow(true)}>Info</button>
+              <Modal onClose={() => setShow(false)} show = {show}/>
               </li>
             </ul>
           </nav>
           <Switch>
-            <Route path="/" exact>
-              <div> <h1>Home page</h1> This is the home page</div>
-            </Route>
-            <Route path="/completed">
-              <div> <h1>Completed</h1> Items you have completed are shown here</div>
-            </Route>
-            <Route path="/info">
-              <div> <h1>Info</h1>
-              </div>
-            </Route>
+            <Route path="/" exact component={Home}/>
+            <Route path="/completed" component={Completed}/>
           </Switch>
         </div>
+        
       </BrowserRouter>
+      
     );
   }
-}
 export default App;
---
+++ src/pages/completed.js
@@ -0,0 +1,15 @@
+import React from 'react';
+  
+const Completed = () => {
+  return (
+    <div>
+      <h1>Completed tasks from todo-list</h1>
+      <br></br>
+      <br></br>
+      <p>Test</p>
+      <p>Test2</p>
+    </div>
+  );
+};
+  
+export default Completed;
